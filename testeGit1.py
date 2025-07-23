88e59c6bf8f8afa896b404a71fd03fb9e7f2178d
vini-pontesb <viniciusbragacontatos@gmail.com>
2025-07-23 10:24:08 -0300

    Adicionando while e tabela por 2

--- testeGit1.py
+++ testeGit1.py
@@ -1,9 +1,14 @@
-n1 = int(input("Adicione um número inteiro: "))
-n2 = int(input("Adicione outro número inteiro: "))
+n1 = int(input("Adicione um número par para ser calculado com o número 2: "))
+n2 = 2
+resto = n1 % n2
 soma = n1 + n2
 sub = n1 - n2
 multi = n1 * n2
 div = n1 / n2
+while resto != 0:
+    print("Você não adicionou um número par, tente novamente:")
+    n1 = int(input("Adicione um número par para ser calculado com o número 2: "))
+    resto = n1 % n2
 print(f"A tabela entre os números {n1} e {n2} ficou da seguinte forma: ")
 print(f"soma: {soma}")
 print(f"subtração: {sub}")
