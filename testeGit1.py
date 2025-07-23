122da5284ab95ddfa257fe91388faa561e0a95d9
vini-pontesb <viniciusbragacontatos@gmail.com>
2025-07-23 10:02:38 -0300

    Criando tabela

--- testeGit1.py
+++ testeGit1.py
@@ -1,13 +1,11 @@
-print("Olá Mundo!")
-print("Lorem ipsum")
-n1 = 2
-n2 = 4
+n1 = int(input("Adicione um número inteiro: "))
+n2 = int(input("Adicione outro número inteiro: "))
 soma = n1 + n2
 sub = n1 - n2
 multi = n1 * n2
 div = n1 / n2
-print(soma)
-print(sub)
-print(multi)
-print(div)
-print("branch3")
+print(f"A tabela entre os números {n1} e {n2} ficou da seguinte forma: ")
+print(f"soma: {soma}")
+print(f"subtração: {sub}")
+print(f"multiplicação: {multi}")
+print(f"divisão: {div}")
